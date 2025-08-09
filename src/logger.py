0d254074fba839b59d423cbca61391ee2f09dc34
K.Sielski <kris@sielski.com>
2025-08-08 20:25:55 -0400

    Disable wiriting log to screen basedn on yaml setting

--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,16 @@ import logging
 import os
 from typing import Optional
 
-def setup_logger(name: str, level: str = "INFO", log_file: Optional[str] = None) -> logging.Logger:
+def setup_logger(name: str, level: str = "INFO", log_file: Optional[str] = None, console: bool = True) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(level)
     formatter = logging.Formatter('[%(asctime)s] %(levelname)s [%(name)s]: %(message)s')
 
-    # Console handler
-    ch = logging.StreamHandler()
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    # Console handler (optional)
+    if console:
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
 
     # File handler
     if log_file:
